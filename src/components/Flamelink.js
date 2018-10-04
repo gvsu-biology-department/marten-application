8a6ce0012aff00d9d7a25702b70e70c6ebeab0de
ajmaley <maleya@mail.gvsu.edu>
2018-10-04 19:22:31 -0400

    Prepared feature for production

--- src/components/Flamelink.js
+++ src/components/Flamelink.js
@@ -1,13 +1,8 @@
 import { Component } from 'react';
-import flamelinkApp from '../flamelink.js';
 
 class Flamelink extends Component {
     render() {
 
-        //Grabs data from text field in entry from the martenSchemaDemo
-        flamelinkApp.content.get('martenSchemaDemo')
-        .then(flameData => document.getElementById("flamelinkDemo").innerHTML = flameData.field_1538162314419);
-        
         return(null);
     }
 }

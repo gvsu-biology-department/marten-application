49eaa2a4f453994a8479bfbeb3be89a3fa6cadec
Al Duncanson <alexjduncanson@gmail.com>
2018-10-26 17:43:09 -0400

    added sighting detail map

--- src/components/SightingDetail.js
+++ src/components/SightingDetail.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import Disqus from 'disqus-react';
 import Divider from '@material-ui/core/Divider';
+import SightingDetailMap from './SightingDetailMap';
 
 class SightingDetail extends Component {
 
@@ -21,6 +22,7 @@ class SightingDetail extends Component {
                 <p>{`When: ${this.props.detail.date}, ${this.props.detail.time}`}</p>
                 <p>{`Where: ${this.props.detail.lat} degrees N, and ${this.props.detail.lng} degrees E`}</p>
                 <p>{`${this.props.detail.desc}`}</p>
+                <SightingDetailMap lat={this.props.detail.lat} lng={this.props.detail.lng}/>
 
                 <Disqus.DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
             </Fragment>

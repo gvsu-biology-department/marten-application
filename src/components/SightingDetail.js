4d0188d144e9cee804bc190061a2529967f95eb2
Al Duncanson <alexjduncanson@gmail.com>
2018-10-25 21:26:18 -0400

    added sighting list, and custom comment thread

---
+++ src/components/SightingDetail.js
@@ -0,0 +1,27 @@
+import React, { Component, Fragment } from 'react';
+import Disqus from 'disqus-react';
+
+class SightingDetail extends Component {
+
+    componentDidUpdate() {
+        window.history.pushState("", "", this.props.detail.id);
+    }
+
+    render() {
+        const disqusShortname = 'https-marten-application-netlify-com';
+        const disqusConfig = {
+            url: `http://localhost:3000/${this.props.detail.id}`,
+            identifier: this.props.detail.id,
+            title: this.props.detail.id
+        }
+
+        return (
+            <Fragment>
+                <p>{this.props.detail.type}</p>
+                <Disqus.DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+            </Fragment>
+        );
+    }
+}
+
+export default SightingDetail;

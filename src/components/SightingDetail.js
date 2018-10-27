75438399851c716783c6455cb7ec61ad88b3bf3e
Al Duncanson <alexjduncanson@gmail.com>
2018-10-27 16:08:53 -0400

    best I got right now

--- src/components/SightingDetail.js
+++ src/components/SightingDetail.js
@@ -1,6 +1,5 @@
 import React, { Component, Fragment } from 'react';
 import Disqus from 'disqus-react';
-import Divider from '@material-ui/core/Divider';
 import SightingDetailMap from './SightingDetailMap';
 
 class SightingDetail extends Component {
@@ -15,15 +14,13 @@ class SightingDetail extends Component {
 
         return (
             <Fragment>
-                <h1>{`Sighting ${this.props.detail.id}`}</h1>
-                <Divider/>
-                <p>{`Type: ${this.props.detail.type}`}</p>
-                <p>{`Confidence: ${this.props.detail.confidence}`}</p>
-                <p>{`When: ${this.props.detail.date}, ${this.props.detail.time}`}</p>
-                <p>{`Where: ${this.props.detail.lat} degrees N, and ${this.props.detail.lng} degrees E`}</p>
-                <p>{`${this.props.detail.desc}`}</p>
                 <SightingDetailMap lat={this.props.detail.lat} lng={this.props.detail.lng}/>
-
+                <div className='sighting-details'>
+                    <p>{`Confidence: ${this.props.detail.confidence}`}</p>
+                    <p>{`When: ${this.props.detail.date}, ${this.props.detail.time}`}</p>
+                    <p>{`Where: ${this.props.detail.lat} degrees N, and ${this.props.detail.lng} degrees E`}</p>
+                    <p>{`${this.props.detail.desc}`}</p>
+                </div>
                 <Disqus.DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
             </Fragment>
         );

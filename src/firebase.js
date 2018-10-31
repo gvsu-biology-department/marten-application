d975fbfe2af61e818a98902f071d8f44326feabc
ajmaley <maleya@mail.gvsu.edu>
2018-10-30 20:20:05 -0400

    Initializing firebaseApp using firebase-admin sdk

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
-import firebase from 'firebase/app';
-import 'firebase/database';
-import 'firebase/storage';
 import * as admin from 'firebase-admin';
 
 var serviceAccount = require('./keys/marten-application-firebase-adminsdk-zvjmp-c177ac648f.json');
 
-const firebaseApp = admin.initializeApp({
-                      credential: admin.credential.cert(serviceAccount),
-                      databaseURL: 'https://marten-application.firebaseio.com'
-                    });
+const firebaseConfig = {
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://marten-application.firebaseio.com",
+  storageBucket: "marten-application.appspot.com"      
+};
 
-export default firebase;
+const firebaseApp = admin.initializeApp(firebaseConfig);
+
+export default 'firebase-admin';

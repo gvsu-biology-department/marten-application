4fba3db3dbf641ab9b86f0d669a2d029e0919773
ajmaley <maleya@mail.gvsu.edu>
2018-11-01 21:19:34 -0400

    Added FlameLink to Homepage.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Typography from '@material-ui/core/Typography';
+import FlameLinkComponentCreations from '../components/FlameLinkComponentCreations';
+import flamelinkApp from '../flamelink.js';
 
 class Home extends Component {
+    constructor() {
+        super();
+
+        global.schemaName = 'martenInfo';
+
+        this.state = {
+          schemaDetails: '',
+        }
+
+        flamelinkApp.schemas.getFields(global.schemaName, { fields: [ 'title', 'key', 'type', 'gridColumns', 'description', 'options' ] })
+        .then(result => this.setState({
+          schemaDetails: result
+        }))
+      }
 
     render() {
         return (
@@ -9,6 +25,9 @@ class Home extends Component {
                 <Typography variant='display1' align='center' gutterBottom>
                     Home
                 </Typography>
+                <Fragment>
+                    <FlameLinkComponentCreations schemaDetails = {this.state.schemaDetails}/>
+                </Fragment>
             </div>
         );
     }

2cb53178df77f91dd035c447cdd3a53857951828
wildscotsmen <thewildscotsmen@gmail.com>
2018-11-08 21:59:38 -0500

    Fixed issue.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+document.title = 'Marten Tracker';
+
 ReactDOM.render(<App/>, document.getElementById('root'));
 
 registerServiceWorker();

4b56907006ebba8df89ef7ec7765bd9c20239859
wildscotsmen <thewildscotsmen@gmail.com>
2018-11-08 22:11:08 -0500

    Rearranged code.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-document.title = 'Marten Tracker | Home';
-
 ReactDOM.render(<App/>, document.getElementById('root'));
+
+document.title = "Marten Tracker | Home";
 
 registerServiceWorker();

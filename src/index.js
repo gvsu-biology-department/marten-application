86580d4a4018436c3570b292168a3bb3f158aeb8
wildscotsmen <thewildscotsmen@gmail.com>
2018-11-08 22:01:29 -0500

    Made title in index.js more specific to first page.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-document.title = 'Marten Tracker';
+document.title = 'Marten Tracker | Home';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 

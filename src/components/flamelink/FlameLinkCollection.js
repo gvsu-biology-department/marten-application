3feec479f2eeb82a05c908ec8071df9310680b70
wildscotsmen <thewildscotsmen@gmail.com>
2018-12-10 13:58:01 -0500

    Initial feature commit.

--- src/components/flamelink/FlameLinkCollection.js
+++ src/components/flamelink/FlameLinkCollection.js
@@ -1,52 +1,49 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import flamelinkApp from '../../utilities/flamelink.js';
 import FlameLinkCollectionComponentCreations from './FlameLinkCollectionComponentCreations';
 
 class FlameLinkCollection extends Component {
-    constructor() {
-        super();
-        
+    state = {
+        schemaContent: '',
+    }
 
-        this.state = {
-          schemaContent: '',
-        }
+    componentDidMount() {
+        flamelinkApp.content.get(this.props.schemaName)
+            .then(result => this.setState({
+                schemaContent: result
+            }))
+    }
 
-        flamelinkApp.content.get(global.schemaName)
-        .then(result => this.setState({
-          schemaContent: result
-        }))
-      }
-
-    getCollectionContent(schemaData){
+    getCollectionContent(schemaData) {
         var arr2 = [];
         var collectionInfo = [schemaData, this.state.schemaContent];
-        for (var val in this.state.schemaContent){
+        for (var val in this.state.schemaContent) {
             arr2[this.state.schemaContent[val]['order']] = val;
         }
         return arr2.map(this.getCollectionComponentInfo, collectionInfo);
     }
 
-    getCollectionComponentInfo(num){
+    getCollectionComponentInfo(num) {
         var arr3 = [];
-        for (var val in this[0]){
+        for (var val in this[0]) {
             arr3.push(val);
         }
         return <FlameLinkCollectionComponentCreations schemaData={this[0]} schemaContent={this[1][num]} arr={arr3} key={num} />
     }
 
     countProperties(obj) {
-    var count = 0;
+        var count = 0;
 
-    for(var prop in obj) {
-        if(obj.hasOwnProperty(prop))
-            ++count;
+        for (var prop in obj) {
+            if (obj.hasOwnProperty(prop))
+                ++count;
+        }
+
+        return count;
     }
 
-    return count;
-}
-
     render() {
-        return(
+        return (
             <div>
                 {this.getCollectionContent(this.props.schemaData)}
             </div>
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,43 +1,72 @@
 import React, { Component, Fragment } from 'react';
 import FlameLinkComponentCreations from '../components/flamelink/FlameLinkComponentCreations';
 import flamelinkApp from '../utilities/flamelink.js';
+import PropTypes from 'prop-types';
+import { Typography } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+    header: {
+        paddingRight: 20,
+        paddingLeft: 20,
+        paddingTop: 20,
+    },
+});
 
 class About extends Component {
-    constructor() {
-        super();
-
-        global.schemaName = 'martenAbout';
-
-        this.state = {
-          schemaDetails: '',
-          schemaType: '',
-        }
-
-        flamelinkApp.schemas.getFields(global.schemaName, { fields: [ 'title', 'key', 'type', 'gridColumns', 'description', 'options' ] })
-        .then(result => this.setState({
-          schemaDetails: result
-        }))
-        
-        flamelinkApp.schemas.get(global.schemaName)
-        .then(result => this.setState({
-          schemaType: result.type
-        }))
+    state = {
+        researcherSchemaName: 'martenAbout',
+        developerSchemaName: 'martenAboutDev',
+        researcherSchemaDetails: '',
+        researcherSchemaType: '',
+        developerSchemaDetails: '',
+        developerSchemaType: ''
     }
 
     componentDidMount() {
         document.title = 'Marten Tracker | About';
+        
+        
+        // Pulling in schema details for researchers
+        flamelinkApp.schemas.getFields(this.state.researcherSchemaName, { fields: ['title', 'key', 'type', 'gridColumns', 'description', 'options'] })
+            .then(result => this.setState({
+                researcherSchemaDetails: result
+            }))
+
+        flamelinkApp.schemas.get(this.state.researcherSchemaName)
+            .then(result => this.setState({
+                researcherSchemaType: result.type
+            }))
+
+
+        // Pulling in schema details for developers
+        flamelinkApp.schemas.getFields(this.state.developerSchemaName, { fields: ['title', 'key', 'type', 'gridColumns', 'description', 'options'] })
+            .then(result => this.setState({
+                developerSchemaDetails: result
+            }))
+
+        flamelinkApp.schemas.get(this.state.developerSchemaName)
+            .then(result => this.setState({
+                developerSchemaType: result.type
+            }))
     }
 
     render() {
-        
+        const { classes } = this.props;
+
         return (
-            <div>
-                <Fragment>
-                    <FlameLinkComponentCreations schemaDetails = {this.state.schemaDetails} schemaType = {this.state.schemaType}/>
-                </Fragment>
-            </div>
+            <Fragment>
+                <Typography variant="display1" className={classes.header}>Researchers</Typography>
+                <FlameLinkComponentCreations schemaDetails={this.state.researcherSchemaDetails} schemaType={this.state.researcherSchemaType} schemaName={this.state.researcherSchemaName} />
+                <Typography variant="display1" className={classes.header}>Developers</Typography>
+                <FlameLinkComponentCreations schemaDetails={this.state.developerSchemaDetails} schemaType={this.state.developerSchemaType} schemaName={this.state.developerSchemaName} />
+            </Fragment>
         );
     }
 }
 
-export default About;
+About.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(About);
